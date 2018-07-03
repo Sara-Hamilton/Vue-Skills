bdf19b63b5c2fa2685e5f72988e748c4e25310ca
Sara Hamilton <sara@indeterminate.net>
2018-07-02 17:00:51 -0700

    add styling to app.vue and skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="skills">
+  <div class="container">
     <div class="holder">
       <ul>
         <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
@@ -8,7 +8,7 @@
       <p v-if="skills.length >= 2">You have more than one skill.</p>
       <p v-else>You have less than or equal to one skill.</p>
 
-      <div v-bind:style="alertObject">test</div>
+      <!-- <div v-bind:style="alertObject">test</div> -->
     </div>
   </div>
 </template>
@@ -33,6 +33,36 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src="./Skills.css" scoped>
+<!-- <style src="./Skills.css" scoped> -->
+<style scoped>
+
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align: center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 
 </style>
